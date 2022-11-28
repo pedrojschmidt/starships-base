323773208ad35e953832d80c6a7852b7a3fae614
pedrojschmidt <pedro.schmidt@ing.austral.edu.ar>
2022-11-28 11:54:30 -0300

    Cambios

--- app/src/main/java/starships/gameObjects/Spaceship.java
+++ app/src/main/java/starships/gameObjects/Spaceship.java
@@ -8,30 +8,24 @@ public class Spaceship extends GameObject {
     private final int pointsWhenDestroyed;
     private final int bulletsPerShot;
 
-    public Spaceship(Player player, int pointsWhenDestroyed, String id, ObjectStyle style, ObjectSize size, Vector position, Vector direction, int speed, int rotationDegrees, int damage, int health, boolean isVisible, int bulletsPerShot) {
+    public Spaceship(String id, ObjectStyle style, ObjectSize size, Vector position, Vector direction, int speed, int rotationDegrees, int damage, int health, boolean isVisible, Player player, int pointsWhenDestroyed, int bulletsPerShot) {
         super(id, ObjectType.SPACESHIP, style, ObjectShape.TRIANGULAR, size, position, direction, speed, rotationDegrees, damage, health, isVisible);
         this.player = player;
         this.pointsWhenDestroyed = pointsWhenDestroyed;
         this.bulletsPerShot = bulletsPerShot;
     }
 
-//    public Spaceship newSpaceship(){
-//
-//    }
-
     public Spaceship move(Vector direction, int speed){
-        return (Spaceship) super.changeDirSpd(direction, speed);
+        return new Spaceship(super.getId(), super.getStyle(), super.getSize(), super.getPosition(), direction, speed, super.getRotationDegrees(), super.getDamage(), super.getHealth(), super.isVisible(), player, pointsWhenDestroyed, bulletsPerShot);
     }
 
     public Spaceship rotate(int rotationDegrees){
-        return (Spaceship) super.setRotation(rotationDegrees);
+        return new Spaceship(super.getId(), super.getStyle(), super.getSize(), super.getPosition(), super.getDirection(), super.getSpeed(), rotationDegrees, super.getDamage(), super.getHealth(), super.isVisible(), player, pointsWhenDestroyed, bulletsPerShot);
     }
 
     public Spaceship resetPosition(){
-        return (Spaceship) super.changePosDirRot(new Vector(300, 300), new Vector(0, 1), 180);
+        return new Spaceship(super.getId(), super.getStyle(), super.getSize(), new Vector(300, 300), new Vector(0, 1), 0, 180, super.getDamage(), super.getHealth(), super.isVisible(), player, pointsWhenDestroyed, bulletsPerShot);
     }
-
-
 
     public Player getPlayer() {
         return player;
