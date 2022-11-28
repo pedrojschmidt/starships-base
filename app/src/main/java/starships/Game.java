2c3a4668e7a1b9f5aa530fe2ede8965fa992808e
pedrojschmidt <pedro.schmidt@ing.austral.edu.ar>
2022-11-28 12:46:27 -0300

    Agregado configuration

---
+++ app/src/main/java/starships/Game.java
@@ -0,0 +1,15 @@
+package starships;
+
+import starships.configuration.Configuration;
+
+import java.util.List;
+
+public class Game {
+
+    private List<Player> players;
+    private List<GameObject> objects;
+
+    private Configuration configuration;
+
+    private boolean isPaused;
+}
---
+++ app/src/main/java/starships/configuration/Configuration.java
@@ -0,0 +1,115 @@
+package starships.configuration;
+
+import javafx.scene.input.KeyCode;
+import starships.ObjectStyle;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Configuration {
+
+    private final int amountOfPlayers;
+    private final int livesPerPlayer;
+    private final Map<String, KeyCode> keyboardConfiguration;
+    private final Map<String, ObjectStyle> styles;
+
+    public Configuration() {
+        List<String> lines = getLines("app\\src\\main\\java\\starships\\configuration\\ConfigurationFile");
+        Map<String, String> optionsMap = generateMap(lines);
+
+        amountOfPlayers = Integer.parseInt(optionsMap.get("amountOfPlayers"));
+        livesPerPlayer = Integer.parseInt(optionsMap.get("amountOfLives"));
+        keyboardConfiguration = getKeyBoardMap(optionsMap.get("keyBoardSettings"));
+        styles = getStyles(optionsMap.get("styles"));
+    }
+
+    private Map<String, String> generateMap(List<String> lines) {
+        Map<String, String> map = new HashMap<>();
+        for (String line : lines){
+            String[] split = line.split(":");
+            map.put(split[0], split[1]);
+        }
+        return map;
+    }
+
+    private Map<String, KeyCode> getKeyBoardMap(String keyBoardSettings) {
+        Map<String, KeyCode> map = new HashMap<>();
+        String[] split = keyBoardSettings.split(";");
+        for (String s : split) {
+            String[] innerSplit = s.split("=");
+            map.put(innerSplit[0], getKeyCode(innerSplit[1]));
+        }
+        return map;
+    }
+
+    private KeyCode getKeyCode(String str){
+        return switch (str){
+            case "W" -> KeyCode.W;
+            case "S" -> KeyCode.S;
+            case "A" -> KeyCode.A;
+            case "D" -> KeyCode.D;
+            case "Q" -> KeyCode.Q;
+            case "E" -> KeyCode.E;
+            case "SPACE" -> KeyCode.SPACE;
+            case "U" -> KeyCode.U;
+            case "J" -> KeyCode.J;
+            case "H" -> KeyCode.H;
+            case "K" -> KeyCode.K;
+            case "Y" -> KeyCode.Y;
+            case "I" -> KeyCode.I;
+            case "ENTER" -> KeyCode.ENTER;
+        };
+    }
+
+    private Map<String, ObjectStyle> getStyles(String styles) {
+        Map<String, ObjectStyle> map = new HashMap<>();
+        String[] split = styles.split(";");
+        for (String s : split) {
+            String[] innerSplit = s.split("=");
+            map.put(innerSplit[0], getStyle(innerSplit[1]));
+        }
+        return map;
+    }
+
+    private ObjectStyle getStyle(String str) {
+        return switch (str){
+            case "SPACESHIP1" -> ObjectStyle.SPACESHIP1;
+            case "SPACESHIP2" -> ObjectStyle.SPACESHIP2;
+            case "SPACESHIP3" -> ObjectStyle.SPACESHIP3;
+        };
+    }
+
+    public List<String> getLines(String directory) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(directory))){
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    public int getAmountOfPlayers() {
+        return amountOfPlayers;
+    }
+
+    public int getLivesPerPlayer() {
+        return livesPerPlayer;
+    }
+
+    public Map<String, KeyCode> getKeyboardConfiguration() {
+        return keyboardConfiguration;
+    }
+
+    public Map<String, ObjectStyle> getStyles() {
+        return styles;
+    }
+}
